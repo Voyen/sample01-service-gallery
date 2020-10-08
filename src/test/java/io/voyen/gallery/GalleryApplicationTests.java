154e751992c63706fdbd460d265669c5b31fa13b
Voyen <solitary_confinement21@hotmail.com>
2020-10-09 01:29:08 +1100

    Initial commit

---
+++ src/test/java/io/voyen/gallery/GalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.voyen.gallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
